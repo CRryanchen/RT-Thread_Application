b3cd63d9f512255ad94a50f3d8e3e2e92ac070d3
ryan·chen <ryan.cr.chen@gmail.com>
2020-08-20 06:54:37 +0800

    互斥量同步实验，使用互斥量需要在rtconfig,h中使能 RT_USING_MUTEX 宏

--- User/main.c
+++ User/main.c
@@ -14,8 +14,8 @@
 // 定义线程控制块指针
 static rt_thread_t receive_thread = RT_NULL;
 static rt_thread_t send_thread = RT_NULL;
-// 定义信号量控制块
-static rt_sem_t test_sem = RT_NULL;
+// 定义互斥量控制块
+static rt_mutex_t test_mux = RT_NULL;
 
 
 /*
@@ -23,7 +23,7 @@ static rt_sem_t test_sem = RT_NULL;
  *                          全局变量声明
  ******************************************************************
  */
-
+uint8_t ucValue [ 2 ] = { 0x00, 0x00 };
 
 /*
  ******************************************************************
@@ -51,16 +51,15 @@ int main(void)
 	 * 即在 component.c 文件中的 rtthread_startup() 函数中完成了
 	 * 所以在 main 函数中，只需要创建线程和启动线程即可
 	 */
-	rt_kprintf("这是一个[野火]-STM32 全系列开发板 RTT 计数信号量同步实验！\n");
-	rt_kprintf("车位默认值为5个，按下K1申请车位，按下K2释放车位！\n\n");
+	rt_kprintf("这是一个[野火]-STM32 全系列开发板 RTT 互斥量同步实验！\n");
+	rt_kprintf("同步成功则输出Successful,反之输出Fail\n");
 
 	// 创建一个信号量
-	test_sem = rt_sem_create("test_sem",			// 信号量名字
-							5,					// 信号量初始值，默认有一个信号量
-							RT_IPC_FLAG_FIFO);	// 队列模式 FIFO
-	if (test_sem != RT_NULL)
+	test_mux = rt_mutex_create("test_mux",			// 互斥量名字
+							RT_IPC_FLAG_PRIO);	// 队列模式 FIFO
+	if (test_mux != RT_NULL)
 	{
-		rt_kprintf("计数信号量创建成功！\n\n");
+		rt_kprintf("互斥量创建成功！\n\n");
 	}
 
 	receive_thread = 									// 线程控制块指针
@@ -107,25 +106,20 @@ int main(void)
  */
 static void receive_thread_entry(void *parameter)
 {
-	rt_err_t uwRet = RT_EOK;
 	while (1)
 	{
-		if (Key_Scan(KEY1_GPIO_PORT, KEY1_GPIO_PIN) == KEY_ON)
+		rt_mutex_take(test_mux, RT_WAITING_FOREVER);
+		if (ucValue[0] == ucValue[1])
 		{
-			// 获取一个计数信号量
-			uwRet = rt_sem_take(test_sem,
-								0);				// 等待时间：0
-			if (uwRet == RT_EOK)
-			{
-				rt_kprintf( "KEY1被单击：成功申请到停车位。\r\n" );
-			}
-			else
-			{
-				rt_kprintf( "KEY1被单击：不好意思，现在停车场已满！\r\n" );
-			}
-			
+			rt_kprintf ( "Successful\n" );
 		}
-		rt_thread_delay(20);			// 每20ms扫描一次
+		else
+		{
+			rt_kprintf ( "Fail\n" );
+		}
+		rt_mutex_release( test_mux ); 	//释放互斥量
+		
+		rt_thread_delay(1000);
 	}
 }
 
@@ -134,21 +128,12 @@ static void send_thread_entry(void *parameter)
 	rt_err_t uwRet = RT_EOK;
 	while (1)
 	{
-		if (Key_Scan(KEY2_GPIO_PORT, KEY2_GPIO_PIN) == KEY_ON)
-		{
-			// 释放一个计数信号量
-			uwRet = rt_sem_release(test_sem);
-			if (uwRet == RT_EOK)
-			{
-				rt_kprintf ( "KEY2被单击：释放1个停车位。\r\n" );
-			}
-			else
-			{
-				rt_kprintf ( "KEY2被单击：但已无车位可以释放！\r\n" );
-			}
-			
-		}
-		rt_thread_delay(20);			// 每20ms扫描一次
+		rt_mutex_take(test_mux, RT_WAITING_FOREVER);		// 获取互斥量
+		ucValue[0]++;
+		rt_thread_delay(100);								// 延时100ms
+		ucValue[1]++;
+		rt_mutex_release( test_mux ); 						// 释放互斥量
+		rt_thread_yield();									// 放弃剩余时间片，进行一次线程切换
 	}
 }
 
